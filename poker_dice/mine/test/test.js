a7d61c7c43446444626245aead59bf196232c7c0
Alexandre <alexandre.poignand@epitech.digital>
2020-10-31 16:53:14 +0100

    bouton pour arreter la partie quand souahiter

--- poker_dice/mine/test/test.js
+++ poker_dice/mine/test/test.js
@@ -1,4 +1,5 @@
 
+let turnLeft = 3;
 function rollDice() {
 
     for (let dieNumber = 1; dieNumber <= 5; dieNumber++) {
@@ -8,8 +9,35 @@ function rollDice() {
             document.getElementById("die" + dieNumber).innerHTML = dieValue;
         }
     }
+    document.getElementById("turnsLeft").innerHTML = turnLeft;
+    turnLeft--;
+
+    /* if (turnLeft == 0) {
+         document.getElementById("roll").disabled = true;
+     }*/
 }
+
 
 function rollDie() {
     return Math.floor(Math.random() * Math.floor(6) + 1);
 }
+/*
+function numberOfTurn() {
+
+    for (turnleft = 3; turnLeft >= 0; turnLeft--) {
+        document.getElementById("turnLeft").innerHTML = "there is " + turnLeft + " ."
+    }
+
+    */
+function endOfTheGame() {
+    document.getElementById("roll").disabled = true;
+    document.getElementById("information").innerHTML = "The game is over";
+    
+
+
+    }
+/*
+    if (turnLeft == 0) {
+        document.getElementById("roll").disabled = true;
+    }
+}*/
